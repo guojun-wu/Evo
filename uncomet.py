a8651f0b168f0eda30e81bfb10302f5cddf83f82
Byron Wu <wuguoaiw@gmail.com>
2023-11-01 15:29:20 +0100

    smallerbatch

--- uncomet.py
+++ uncomet.py
@@ -17,7 +17,7 @@ class COMET:
                 "ref": ref
             })
         if self.test_mode:
-            data = data[:16]
+            data = data[:2]
 
         model_output = self.model.predict(data, batch_size=16, gpus=1)
         return model_output.scores
