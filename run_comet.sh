b43c3601c82f0c94dea12cea15b4a239d787d895
Byron Wu <wuguoaiw@gmail.com>
2023-11-01 14:06:15 +0100

    log

--- run_comet.sh
+++ run_comet.sh
@@ -4,8 +4,10 @@ set -e
 source /work/ec255/ec255/guojun/evo/bin/activate
 
 export BASE_DIR=/work/ec255/ec255/guojun/Evo
-python main.py \
+python $BASE_DIR/main.py >> $BASE_DIR/log_file.log 2>&1 & \
     --metric=comet \
     --data_path=$BASE_DIR/evo_data \
     --output_path=$BASE_DIR/result \
     --test\
+
+echo "Script has started at $(date)" >> $BASE_DIR/log_file.log
