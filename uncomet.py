ff8b15cf7ee70f27f9aed06d0da143e1ac9e6c26
Byron Wu <wuguoaiw@gmail.com>
2023-11-03 13:53:01 +0100

    path

--- uncomet.py
+++ uncomet.py
@@ -3,7 +3,7 @@ import torch
 
 class COMET:
     def __init__(self, test_mode=False):
-        model_path = "/home/ec255/ec255/guojun/.cache/huggingface/hub/models--Unbabel--wmt22-comet-da/snapshots/371e9839ca4e213dde891b066cf3080f75ec7e72/checkpoints/model.ckpt"
+        model_path = download_model("Unbabel/wmt22-comet-da", "/home/ec255/ec255/guojun/.cache/huggingface/hub/")
         self.model = load_from_checkpoint(model_path)
         self.test_mode = test_mode
         print("Model cache directory:", model_path)
