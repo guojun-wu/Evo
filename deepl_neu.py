e3138ab63af7e45129e6cad576d126b75bc0f981
Byron Wu <wuguoaiw@gmail.com>
2024-02-16 12:50:18 +0100

    no bert

--- deepl_neu.py
+++ deepl_neu.py
@@ -8,11 +8,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source', type=str, default='en', help='source language')
     parser.add_argument('-t', '--target', type=str, default='de', help='target language')
+    parser.add_argument('--test', action='store_true', help='test mode')
 
     args = parser.parse_args()
 
     src_lang = args.source
     tgt_lang = args.target
+    test_mode = args.test
     df = pd.read_csv(f'evo_data/{src_lang}-{tgt_lang}.csv')
     source = df['source'].tolist()
     target = []
@@ -20,13 +22,13 @@ def main():
     with open(deepl_path, 'r', encoding='utf-8') as f:
         target = f.readlines()
 
-    metrics = ['bert', 'comet22', 'unite', 'comet20']
+    metrics = ['comet22', 'unite', 'comet20']
 
     for metric in metrics:
         if metric != 'bert':
-            scorer = COMET(metric, test_mode=True)
+            scorer = COMET(metric, test_mode=test_mode)
         else:
-            scorer = BERTScore(tgt_lang, test_mode=True)
+            scorer = BERTScore(tgt_lang, test_mode=test_mode)
 
         score_df = pd.DataFrame()
         for date in df.iloc[:, 2:]:
